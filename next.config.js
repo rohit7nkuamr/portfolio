7bf05db04e7bfbcb46fc0c55e4557463f7f44374
rohit7nkuamr <rohit7ngod@gmail.com>
2025-06-11 22:04:18 +0530

    fix: improve path handling for GitHub Pages deployment

--- next.config.js
+++ next.config.js
@@ -1,13 +1,15 @@
 /** @type {import('next').NextConfig} */
 const repo = 'portfolio';
+const isGithubPages = process.env.GITHUB_ACTIONS || false;
 
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  basePath: `/${repo}`,
-  assetPrefix: `/${repo}/`,
+  trailingSlash: true,
+  basePath: isGithubPages ? `/${repo}` : '',
+  assetPrefix: isGithubPages ? `/${repo}/` : '/',
 };
 
 module.exports = nextConfig;
