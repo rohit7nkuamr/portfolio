d1761a2806cf3fb717937ef0307d93b735f8d81e
rohit7nkuamr <rohit7ngod@gmail.com>
2025-06-11 21:55:39 +0530

    fix: set basePath/assetPrefix for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,13 @@
 /** @type {import('next').NextConfig} */
+const repo = 'portfolio';
+
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
+  basePath: `/${repo}`,
+  assetPrefix: `/${repo}/`,
 };
 
 module.exports = nextConfig;
